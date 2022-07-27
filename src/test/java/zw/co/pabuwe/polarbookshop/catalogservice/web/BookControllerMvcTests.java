7f181e69602c9f193eb3afd444d69b7577547727
Nicholas Gamunorwa <nicholas.gamunorwa@cassavasmartech.co.zw>
2022-07-27 18:57:11 +0200

    Tests and CI/CD pipeline

---
+++ src/test/java/zw/co/pabuwe/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -0,0 +1,35 @@
+package zw.co.pabuwe.polarbookshop.catalogservice.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import zw.co.pabuwe.polarbookshop.catalogservice.domain.BookNotFoundException;
+import zw.co.pabuwe.polarbookshop.catalogservice.domain.BookService;
+import zw.co.pabuwe.polarbookshop.catalogservice.web.BookController;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
+class BookControllerMvcTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenGetBookNotExistingThenReturn404() throws Exception {
+        String isbn = "2233445566";
+        given(bookService.viewBookDetails(isbn))
+                .willThrow(BookNotFoundException.class);
+        mockMvc
+                .perform(get("/book/" + isbn))
+                .andExpect(status().isNotFound());
+    }
+
+}
---
+++ src/test/java/zw/co/pabuwe/polarbookshop/catalogservice/web/BookValidationUnitTests.java
@@ -0,0 +1,42 @@
+package zw.co.pabuwe.polarbookshop.catalogservice.web;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import zw.co.pabuwe.polarbookshop.catalogservice.domain.Book;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BookValidationUnitTests {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setup() {
+        try (ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
+            validator = validatorFactory.getValidator();
+        }
+    }
+
+    @Test
+    void whenAllFieldsCorrectThenValidationSucceeds() {
+        var book = new Book("1234567890", "Title", "Author", 9.99);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    void whenIsbnDefinedButIncorrectThenValidationFails() {
+        var book = new Book("a234567890", "Title", "Author", 9.99);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getMessage())
+                .isEqualTo("The ISBN format must be valid.");
+    }
+
+}
