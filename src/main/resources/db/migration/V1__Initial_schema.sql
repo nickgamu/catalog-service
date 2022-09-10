10436d5b35ad814bb7319238c4d9b8b24f4139bf
Nicholas Gamunorwa <nicholas.gamunorwa@cassavasmartech.co.zw>
2022-09-10 18:45:09 +0200

    Test

--- src/main/resources/db/migration/V1__Initial_schema.sql
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -1,10 +1,13 @@
 CREATE TABLE book (
-      id                  BIGSERIAL PRIMARY KEY NOT NULL,
-      author              varchar(255) NOT NULL,
-      isbn                varchar(255) UNIQUE NOT NULL,
-      price               float8 NOT NULL,
-      title               varchar(255) NOT NULL,
-      created_date        timestamp NOT NULL,
-      last_modified_date  timestamp NOT NULL,
-      version             integer NOT NULL
+    id                  BIGSERIAL PRIMARY KEY NOT NULL,
+    author              varchar(255) NOT NULL,
+    isbn                varchar(255) UNIQUE NOT NULL,
+    price               float8 NOT NULL,
+    title               varchar(255) NOT NULL,
+    publisher           varchar(255),
+    created_date        timestamp NOT NULL,
+    created_by          varchar(255),
+    last_modified_date  timestamp NOT NULL,
+    last_modified_by    varchar(255),
+    version             integer NOT NULL
 );
